3689cd61a7cc5d91f8e0541fe038c2c40a48b7b9
Glenn Martinez <gmart@bu.edu>
2019-12-08 12:10:53 -0500

    Add files via upload

---
+++  get_freq_chirp.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import struct
+
+frame_rate = 48000.0
+ 
+infile = "chirp_16khz_mono_2.wav"
+ 
+num_samples = 48000
+ 
+wav_file = wave.open(infile, 'r')
+ 
+data = wav_file.readframes(num_samples)
+ 
+wav_file.close()
+
+data = struct.unpack('{n}h'.format(n=num_samples), data)
+ 	
+data = np.array(data)
+
+data_fft = np.fft.fft(data)
+
+frequencies = np.abs(data_fft)
+
+print("The frequency is {} Hz".format(np.argmax(frequencies)))
+
+plt.subplot(2,1,1)
+ 
+plt.plot(data[:60000])
+ 
+plt.title("Original audio wave")
+plt.ylabel("Amplitude")
+plt.xlabel("Time (sec)")
+
+ 
+plt.subplot(2,1,2)
+
+plt.plot(frequencies)
+ 
+plt.title("Frequencies found")
+ 
+plt.xlim(0,16000)
+plt.subplots_adjust(hspace = 1)
+plt.xlabel("Frequency (Hz)")
+plt.show()
